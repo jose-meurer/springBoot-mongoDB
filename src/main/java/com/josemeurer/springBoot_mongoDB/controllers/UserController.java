cc6cf64560280f68761a4a8365a2e174b68537bc
Jose Meurer <josewmeurer@gmail.com>
2025-01-14 15:50:47 -0300

    Implementado findById no UserService e UserController

--- src/main/java/com/josemeurer/springBoot_mongoDB/controllers/UserController.java
+++ src/main/java/com/josemeurer/springBoot_mongoDB/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.josemeurer.springBoot_mongoDB.dtos.UserDTO;
 import com.josemeurer.springBoot_mongoDB.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,11 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> findAll() {
         List<UserDTO> users = userService.findAll();
         return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        UserDTO dto = userService.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
--- src/main/java/com/josemeurer/springBoot_mongoDB/services/UserService.java
+++ src/main/java/com/josemeurer/springBoot_mongoDB/services/UserService.java
@@ -2,10 +2,12 @@ package com.josemeurer.springBoot_mongoDB.services;
 
 import com.josemeurer.springBoot_mongoDB.dtos.UserDTO;
 import com.josemeurer.springBoot_mongoDB.entities.User;
+import com.josemeurer.springBoot_mongoDB.exceptions.ObjectNotFoundException;
 import com.josemeurer.springBoot_mongoDB.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -19,5 +21,11 @@ public class UserService {
     public List<UserDTO> findAll() {
         List<User> list = userRepository.findAll();
         return list.stream().map(UserDTO::new).toList();
+    }
+
+    public UserDTO findById(String id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        User user = optionalUser.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+        return new UserDTO(user);
     }
 }
